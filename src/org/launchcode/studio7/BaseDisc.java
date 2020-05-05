706c93c493ffcaa28e96e68f50bba296df5f35b8
mmille014 <miguel_miller@cable.comcast.com>
2020-05-05 16:00:33 -0400

    Still in progress

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,25 @@
+//package org.launchcode.studio7;
+//
+//public class BaseDisc implements OpticalDisc {
+//    private int spinSpeed;
+//    private boolean isReadable;
+//
+//    public BaseDisc(int spinSpeed, boolean isReadable) {this.spinSpeed = spinSpeed; this.isReadable = isReadable}
+//    public int getSpinSpeed() {return spinSpeed;}
+//    public void setSpinSpeed(int spinSpeed) {this.spinSpeed = spinSpeed}
+//    public boolean isReadable() {return  isReadable;}
+//
+//    public void setReadable(boolean readable) {isReadable = readable;}
+//    public void readDisk(){
+//
+//    }
+//    public void storesData() {
+//
+//    }
+//    public void getsData() {
+//
+//    }
+//    public void properties() {
+//
+//    }
+//}
--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -1,8 +1,26 @@
 package org.launchcode.studio7;
 
-public class CD {
+public class CD implements OpticalDisc {
+    private double storageCapacity;
 
-    // TODO: Implement your custom interface.
+        // TODO: Implement your custom interface.
+        public void readDisk() {
+
+        }
+
+        public void storesData() {
+
+        }
+
+
+        public void getsData() {
+
+        }
+
+
+        public void properties() {
+
+        }
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
